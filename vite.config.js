8ce5fb882ed3b13b3c630cd893b5a4314c0d6035
Sean <sobrien26@gmail.com>
2026-07-21 16:27:33 -0400

    setting up as a redirect for personal website url, added vite config base

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,6 @@ import { fileURLToPath } from 'node:url';
 import { playwright } from '@vitest/browser-playwright';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: './',
 });
